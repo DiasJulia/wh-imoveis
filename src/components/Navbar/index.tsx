8fbed5db6028bf8c3d3a31b41fdf07cfbc4cd45e
diasjulia <jedl@cin.ufpe.br>
2023-01-18 20:02:22 -0300

    Feat(Routing Structure): Navbar routing reference to neighborhoods

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
 
+import imoveis from '../../data/imoveis.json';
+
 const Navbar = () => {
+    const bairros =  Array.from(new Set(imoveis.map((imovel)=>{ if(!(imovel.bairro in imoveis)) return imovel.bairro})));
   return (
     <nav>
         <Link href="/">Logo</Link>
         <ul>
             <li>
                 <Link href="/imoveis">Imóveis</Link>
+                <ul>{bairros.map((bairro) =>{
+                return <li key={bairro}>
+                        <Link href={`/${encodeURIComponent(bairro || "")}`}>{bairro}</Link>
+                        </li>
+                })}</ul>
             </li>
             <li>
                 <Link href="/">Sobre mim</Link>
