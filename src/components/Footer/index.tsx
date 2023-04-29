0fab67065d3799fce135de759b5b692f548d895c
diasjulia <jedl@cin.ufpe.br>
2023-04-29 03:53:15 -0300

    feat: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
-import {Container} from './styles';
+import {Container, Logo, Navigation} from './styles';
 
 const Footer = () => {
     const bairros = ["Jatiúca", "Cruz das Almas", "Ponta Verde"];
   return (
     <Container>
-    <nav>
+    <Navigation>
         <ul>
             <li>
                 <Link href="/imoveis">Imóveis</Link>
             </li>
+            <ul>
             {bairros.map((bairro) => {
                 return <li key={bairro}>
                     <Link href={`/imoveis?bairro=${encodeURIComponent(bairro)}`}>{bairro}</Link>
                 </li>
                 })}
+            </ul>
         </ul>
-    </nav>
+    </Navigation>
+    <Logo></Logo>
+    <div></div>
     </Container>
   )
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: #09346b;
+  width: 100%;
+  padding: 2em max(calc(100vw - 1400px) / 2, 1rem);
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Navigation = styled.nav`
+  color: #fff;
+  ul {
+    list-style-type: none;
+  }
+
+  ul ul {
+    margin-left: 10px;
+    color: #d9d9d9;
+  }
+`;
+
+export const Logo = styled.div`
+  border-radius: 50%;
+  background-color: #fff;
+  width: 100px;
+  height: 100px;
+`;
